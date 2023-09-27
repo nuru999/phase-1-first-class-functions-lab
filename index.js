701198e6b592b16732000a2838cca5a68aaa551f
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-09-27 09:32:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers - Returns the first two drivers in an array
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers - Returns the last two drivers in an array
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers - Array containing the two functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier - Higher-order function that returns a fare multiplier function
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler - Doubles the fare
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler - Triples the fare
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers - Selects drivers based on a given function
+  const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+  };
+  
